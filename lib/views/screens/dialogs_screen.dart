d1b7a070a1b88b6e73cd2fb9dcc0588244b14d6b
Kagema Njoroge <reecejames934@gmail.com>
2025-03-21 20:01:57 +0300

    Refactor DialogsScreen: move _showDialog method to the end of the class and remove unused UrlNewTabLauncher widget

--- lib/views/screens/dialogs_screen.dart
+++ lib/views/screens/dialogs_screen.dart
@@ -5,24 +5,9 @@ import 'package:web_admin/generated/l10n.dart';
 import 'package:web_admin/theme/theme_extensions/app_button_theme.dart';
 import 'package:web_admin/views/widgets/card_elements.dart';
 import 'package:web_admin/views/widgets/portal_master_layout/portal_master_layout.dart';
-import 'package:web_admin/views/widgets/url_new_tab_launcher.dart';
 
 class DialogsScreen extends StatelessWidget {
   const DialogsScreen({super.key});
-
-  void _showDialog(BuildContext context, DialogType dialogType) {
-    final dialog = AwesomeDialog(
-      context: context,
-      dialogType: dialogType,
-      title: 'Dialog Title',
-      desc: 'Dialog body...',
-      width: kDialogWidth,
-      btnOkOnPress: () {},
-      btnCancelOnPress: () {},
-    );
-
-    dialog.show();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,46 +37,44 @@ class DialogsScreen extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Padding(
-                          padding: EdgeInsets.only(bottom: kDefaultPadding * 2.0),
-                          child: UrlNewTabLauncher(
-                            displayText: 'awesome_dialog - pub.dev',
-                            url: 'https://pub.dev/packages/awesome_dialog',
-                            fontSize: 13.0,
-                          ),
-                        ),
                         Wrap(
                           direction: Axis.horizontal,
                           spacing: kDefaultPadding * 2.0,
                           runSpacing: kDefaultPadding * 2.0,
                           children: [
                             ElevatedButton(
-                              onPressed: () => _showDialog(context, DialogType.info),
+                              onPressed: () =>
+                                  _showDialog(context, DialogType.info),
                               style: appButtonTheme.primaryElevated,
                               child: const Text('Info'),
                             ),
                             ElevatedButton(
-                              onPressed: () => _showDialog(context, DialogType.infoReverse),
+                              onPressed: () =>
+                                  _showDialog(context, DialogType.infoReverse),
                               style: appButtonTheme.primaryElevated,
                               child: const Text('Info Reversed'),
                             ),
                             ElevatedButton(
-                              onPressed: () => _showDialog(context, DialogType.warning),
+                              onPressed: () =>
+                                  _showDialog(context, DialogType.warning),
                               style: appButtonTheme.primaryElevated,
                               child: const Text('Warning'),
                             ),
                             ElevatedButton(
-                              onPressed: () => _showDialog(context, DialogType.error),
+                              onPressed: () =>
+                                  _showDialog(context, DialogType.error),
                               style: appButtonTheme.primaryElevated,
                               child: const Text('Error'),
                             ),
                             ElevatedButton(
-                              onPressed: () => _showDialog(context, DialogType.success),
+                              onPressed: () =>
+                                  _showDialog(context, DialogType.success),
                               style: appButtonTheme.primaryElevated,
                               child: const Text('Success'),
                             ),
                             ElevatedButton(
-                              onPressed: () => _showDialog(context, DialogType.question),
+                              onPressed: () =>
+                                  _showDialog(context, DialogType.question),
                               style: appButtonTheme.primaryElevated,
                               child: const Text('Question'),
                             ),
@@ -107,5 +90,19 @@ class DialogsScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void _showDialog(BuildContext context, DialogType dialogType) {
+    final dialog = AwesomeDialog(
+      context: context,
+      dialogType: dialogType,
+      title: 'Dialog Title',
+      desc: 'Dialog body...',
+      width: kDialogWidth,
+      btnOkOnPress: () {},
+      btnCancelOnPress: () {},
+    );
+
+    dialog.show();
   }
 }
