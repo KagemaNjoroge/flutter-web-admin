cd61cdf85595b15c77e1ca7ec0bf05f6e772f8e7
Kagema Njoroge <reecejames934@gmail.com>
2025-03-21 20:01:43 +0300

    Refactor LoginScreen: extract FormData class, update logo image, and improve code formatting

--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -13,6 +13,11 @@ import 'package:web_admin/theme/theme_extensions/app_color_scheme.dart';
 import 'package:web_admin/utils/app_focus_helper.dart';
 import 'package:web_admin/views/widgets/public_master_layout/public_master_layout.dart';
 
+class FormData {
+  String username = '';
+  String password = '';
+}
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -25,6 +30,157 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formData = FormData();
 
   var _isFormLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final lang = Lang.of(context);
+    final themeData = Theme.of(context);
+
+    return PublicMasterLayout(
+      body: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            padding: const EdgeInsets.only(top: kDefaultPadding * 5.0),
+            constraints: const BoxConstraints(maxWidth: 400.0),
+            child: Card(
+              clipBehavior: Clip.antiAlias,
+              child: Padding(
+                padding: const EdgeInsets.all(kDefaultPadding),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: kDefaultPadding),
+                      child: Image.asset(
+                        'assets/images/new_logo_no_bg.png',
+                        height: 200.0,
+                      ),
+                    ),
+                    Text(
+                      lang.appTitle,
+                      style: themeData.textTheme.headlineMedium!.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(bottom: kDefaultPadding * 2.0),
+                      child: Text(
+                        lang.adminPortalLogin,
+                        style: themeData.textTheme.titleMedium,
+                      ),
+                    ),
+                    FormBuilder(
+                      key: _formKey,
+                      autovalidateMode: AutovalidateMode.disabled,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                bottom: kDefaultPadding * 1.5),
+                            child: FormBuilderTextField(
+                              name: 'username',
+                              decoration: InputDecoration(
+                                labelText: lang.username,
+                                hintText: lang.username,
+                                helperText: '* Demo username: admin',
+                                border: const OutlineInputBorder(),
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always,
+                              ),
+                              enableSuggestions: false,
+                              validator: FormBuilderValidators.required(),
+                              onSaved: (value) =>
+                                  (_formData.username = value ?? ''),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                bottom: kDefaultPadding * 2.0),
+                            child: FormBuilderTextField(
+                              name: 'password',
+                              decoration: InputDecoration(
+                                labelText: lang.password,
+                                hintText: lang.password,
+                                helperText: '* Demo password: admin',
+                                border: const OutlineInputBorder(),
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always,
+                              ),
+                              enableSuggestions: false,
+                              obscureText: true,
+                              validator: FormBuilderValidators.required(),
+                              onSaved: (value) =>
+                                  (_formData.password = value ?? ''),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(bottom: kDefaultPadding),
+                            child: SizedBox(
+                              height: 40.0,
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                style: themeData
+                                    .extension<AppButtonTheme>()!
+                                    .primaryElevated,
+                                onPressed: (_isFormLoading
+                                    ? null
+                                    : () => _doLoginAsync(
+                                          userDataProvider:
+                                              context.read<UserDataProvider>(),
+                                          onSuccess: () =>
+                                              _onLoginSuccess(context),
+                                          onError: (message) =>
+                                              _onLoginError(context, message),
+                                        )),
+                                child: Text(lang.login),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 40.0,
+                            width: double.infinity,
+                            child: TextButton(
+                              style: themeData
+                                  .extension<AppButtonTheme>()!
+                                  .secondaryText,
+                              onPressed: () =>
+                                  GoRouter.of(context).go(RouteUri.register),
+                              child: RichText(
+                                text: TextSpan(
+                                  text: '${lang.dontHaveAnAccount} ',
+                                  style: TextStyle(
+                                    color: themeData.colorScheme.onSurface,
+                                  ),
+                                  children: [
+                                    TextSpan(
+                                      text: lang.registerNow,
+                                      style: TextStyle(
+                                        color: themeData
+                                            .extension<AppColorScheme>()!
+                                            .hyperlink,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   Future<void> _doLoginAsync({
     required UserDataProvider userDataProvider,
@@ -56,10 +212,6 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  void _onLoginSuccess(BuildContext context) {
-    GoRouter.of(context).go(RouteUri.home);
-  }
-
   void _onLoginError(BuildContext context, String message) {
     final dialog = AwesomeDialog(
       context: context,
@@ -73,141 +225,7 @@ class _LoginScreenState extends State<LoginScreen> {
     dialog.show();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final lang = Lang.of(context);
-    final themeData = Theme.of(context);
-
-    return PublicMasterLayout(
-      body: SingleChildScrollView(
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            padding: const EdgeInsets.only(top: kDefaultPadding * 5.0),
-            constraints: const BoxConstraints(maxWidth: 400.0),
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              child: Padding(
-                padding: const EdgeInsets.all(kDefaultPadding),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: kDefaultPadding),
-                      child: Image.asset(
-                        'assets/images/app_logo.png',
-                        height: 80.0,
-                      ),
-                    ),
-                    Text(
-                      lang.appTitle,
-                      style: themeData.textTheme.headlineMedium!.copyWith(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: kDefaultPadding * 2.0),
-                      child: Text(
-                        lang.adminPortalLogin,
-                        style: themeData.textTheme.titleMedium,
-                      ),
-                    ),
-                    FormBuilder(
-                      key: _formKey,
-                      autovalidateMode: AutovalidateMode.disabled,
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: kDefaultPadding * 1.5),
-                            child: FormBuilderTextField(
-                              name: 'username',
-                              decoration: InputDecoration(
-                                labelText: lang.username,
-                                hintText: lang.username,
-                                helperText: '* Demo username: admin',
-                                border: const OutlineInputBorder(),
-                                floatingLabelBehavior: FloatingLabelBehavior.always,
-                              ),
-                              enableSuggestions: false,
-                              validator: FormBuilderValidators.required(),
-                              onSaved: (value) => (_formData.username = value ?? ''),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: kDefaultPadding * 2.0),
-                            child: FormBuilderTextField(
-                              name: 'password',
-                              decoration: InputDecoration(
-                                labelText: lang.password,
-                                hintText: lang.password,
-                                helperText: '* Demo password: admin',
-                                border: const OutlineInputBorder(),
-                                floatingLabelBehavior: FloatingLabelBehavior.always,
-                              ),
-                              enableSuggestions: false,
-                              obscureText: true,
-                              validator: FormBuilderValidators.required(),
-                              onSaved: (value) => (_formData.password = value ?? ''),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: kDefaultPadding),
-                            child: SizedBox(
-                              height: 40.0,
-                              width: double.infinity,
-                              child: ElevatedButton(
-                                style: themeData.extension<AppButtonTheme>()!.primaryElevated,
-                                onPressed: (_isFormLoading
-                                    ? null
-                                    : () => _doLoginAsync(
-                                          userDataProvider: context.read<UserDataProvider>(),
-                                          onSuccess: () => _onLoginSuccess(context),
-                                          onError: (message) => _onLoginError(context, message),
-                                        )),
-                                child: Text(lang.login),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 40.0,
-                            width: double.infinity,
-                            child: TextButton(
-                              style: themeData.extension<AppButtonTheme>()!.secondaryText,
-                              onPressed: () => GoRouter.of(context).go(RouteUri.register),
-                              child: RichText(
-                                text: TextSpan(
-                                  text: '${lang.dontHaveAnAccount} ',
-                                  style: TextStyle(
-                                    color: themeData.colorScheme.onSurface,
-                                  ),
-                                  children: [
-                                    TextSpan(
-                                      text: lang.registerNow,
-                                      style: TextStyle(
-                                        color: themeData.extension<AppColorScheme>()!.hyperlink,
-                                        decoration: TextDecoration.underline,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+  void _onLoginSuccess(BuildContext context) {
+    GoRouter.of(context).go(RouteUri.home);
   }
-}
-
-class FormData {
-  String username = '';
-  String password = '';
 }
